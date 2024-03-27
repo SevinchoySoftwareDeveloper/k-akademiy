f94eb54f83e96c497d5bd8f3f43df86252bbc7d4
Sevinch <159159483+SevinchoySoftwareDeveloper@users.noreply.github.com>
2024-03-27 08:40:39 +0500

    Create type.js
    bosh sahifaga ulanadi

---
+++ bosh sahifa/JavaS/type.js
@@ -0,0 +1,11 @@
+var typed = new Typed(".text", {
+  strings: [
+    "<i>Web Developer</i>",
+    " VOBKENTDAGI BIRINCHI VA <br> KO’PTARMOQLI ZAMONAVY KASBLAR MAKTABIDIR. MARKAZIMIZ <br>  O’ZINING TEXNIK BAZASI HAMDA ZAMONAVIY METODOLIGIYASIGA  <br> EGA.",
+  ],
+  typeSpeed: 50,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: false,
+});
+// bosh sahifa  uchun 
